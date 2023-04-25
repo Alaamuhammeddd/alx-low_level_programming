d2ce060ce7592d7599d5d84d84c5ab3515fd8a5c
Alaamuhammeddd <alaamuhammeddd17@gmail.com>
2023-04-25 08:15:07 -0700

    Added file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: 'sum of two numbers'
+ * Return: sum (Success)
+ * @n1:first num
+ * @n2:second num
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
