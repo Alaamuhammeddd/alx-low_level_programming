3c7e0480fa842fc8d72df5081d84d4e4c6db432f
Alaamuhammeddd <alaamuhammeddd17@gmail.com>
2023-04-25 06:18:55 -0700

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase 
- * Return: 1 for lowercase 0 for otherwise
+ * _islower - checks for lowercase
+ * Return: 1 for lowercase 0 for otherwisie
  */
 int _islower(int c)
 {
         if (c >= 97 && c <= 122)
-                return (1);
-        else
-                return(0);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
