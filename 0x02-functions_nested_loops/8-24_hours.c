92e6c73e1ec473849e99b2b5288d16d0fe1c9b52
Alaamuhammeddd <alaamuhammeddd17@gmail.com>
2023-04-25 08:01:22 -0700

    d

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * jack_bauer - Entry point
- * Description: ''
- * Return: Always 0 (Success)
- * @i: 
+ * Description: 'prints every minute of day'
+ * Return: Void
  */
 void jack_bauer(void)
 {
