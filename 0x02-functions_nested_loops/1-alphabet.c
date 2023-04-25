322ccf394eb951bf2a0a24d6f9a9fbb58670e02d
Alaamuhammeddd <alaamuhammeddd17@gmail.com>
2023-04-25 05:38:27 -0700

    Updated file 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: ''
+ * Description: 'printing alphabet'
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
